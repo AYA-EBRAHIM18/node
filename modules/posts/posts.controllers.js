ed0cf27a7422b8089d088d731cd9dbd353b1853e
AYA-EBRAHIM18 <60238805+AYA-EBRAHIM18@users.noreply.github.com>
2024-06-13 15:36:45 +0300

    Update posts.controllers.js

--- modules/posts/posts.controllers.js
+++ modules/posts/posts.controllers.js
@@ -8,7 +8,7 @@ const getAllPosts = (req, res) => {
   con.execute(
     `select users.id,users.name,posts.id as postId,posts.title,posts.description from users join posts on users.id = posts.user_id`,
     (err, data) => {
-      res.status(200).json({ message: "success", allPosts: data });
+      res.status(200).json({  allPosts: data });
     }
   );
 };
